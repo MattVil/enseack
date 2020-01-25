82cacc8bfb5713580ed92adb61335c0749349a10
mattvil <mattvilain@gmail.com>
2020-01-25 03:02:02 +0100

    try deployment

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_restful import Resource, reqparse, Api
+import json
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+from base import Data, db
+db.init_app(app)
+app.app_context().push()
+db.create_all()
+
+class MyApp(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('data', type=str)
+
+    def get(self, timestamp):
+        item = Data.find_by_timestamp(timestamp)
+        if item:
+            return item.json()
+        return {'Message': 'Movie is not found'}
+
+    def post(self):
+        args = MyApp.parser.parse_args()
+        print("#"*80)
+        print(args['data'])
+        json_data = json.loads(args['data'])
+        item = Data(json_data['timestamp'],
+                    json_data['longitude'],
+                    json_data['latitude'],
+                    json_data['temperature'],
+                    json_data['humidity'],
+                    json_data['sound'],
+                    json_data['co'],
+                    json_data['co2'],
+                    json_data['gpl'],
+                    json_data['waste'])
+        item.save_to()
+        return item.json()
+
+class Find_all(Resource):
+    def get(self):
+        return {'data' : list(map(lambda x: x.json(), Data.query.all()))}
+
+class Root(Resource):
+    def get(self):
+        return {
+            'status': 'OK',
+        }
+
+api.add_resource(Root, '/')
+api.add_resource(Find_all, '/all')
+api.add_resource(MyApp, '/data')
+
+if __name__=='__main__':
+
+    app.run(debug=True)
---
+++ base.py
@@ -0,0 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.BigInteger, unique=False, nullable=False)
+    longitude = db.Column(db.Float, unique=False, nullable=False)
+    latitude = db.Column(db.Float, unique=False, nullable=False)
+    temperature = db.Column(db.Float, unique=False, nullable=False)
+    humidity = db.Column(db.Float, unique=False, nullable=False)
+    sound = db.Column(db.Integer, unique=False, nullable=False)
+    co = db.Column(db.Float, unique=False, nullable=False)
+    co2 = db.Column(db.Float, unique=False, nullable=False)
+    gpl = db.Column(db.Float, unique=False, nullable=False)
+    waste = db.Column(db.Boolean, unique=False, nullable=False)
+
+    def __init__(self, timestamp, longitude, latitude, temperature, humidity, sound, co, co2, gpl, waste):
+        self.timestamp = timestamp
+        self.longitude = longitude
+        self.latitude = latitude
+        self.temperature = temperature
+        self.humidity = humidity
+        self.sound = sound
+        self.co = co
+        self.co2 = co2
+        self.gpl = gpl
+        self.waste = waste
+
+    def json(self):
+        return {'timestamp': self.timestamp,
+                'longitude': self.longitude,
+                'latitude': self.latitude,
+                'temperature': self.temperature,
+                'humidity': self.humidity,
+                'sound': self.sound,
+                'co': self.co,
+                'co2': self.co2,
+                'gpl': self.gpl,
+                'waste': self.waste}
+
+    @classmethod
+    def find_by_timestamp(cls, timestamp):
+        pass
+
+    def save_to(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_(self):
+        db.session.delete(self)
+        db.session.commit()
